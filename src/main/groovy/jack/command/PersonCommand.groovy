1486334adfeaf8aa99482ee3eaa6f7256709ac41
Luisito <le.sastre@ebc.edu.mx>
2021-01-24 16:19:54 -0600

    Add method for convert from person to person command

--- src/main/groovy/jack/command/PersonCommand.groovy
+++ src/main/groovy/jack/command/PersonCommand.groovy
@@ -1,9 +1,21 @@
 package jack.command
 
+import jack.domain.Person
+
 class PersonCommand {
 
+  Long id
   String name
   String lastName
   Integer age
+
+  static PersonCommand toPersonCommand(Person person) {
+    new PersonCommand(
+        id: person.id,
+        name: person.name,
+        lastName: person.lastName,
+        age: person.age
+    )
+  }
 
 }
