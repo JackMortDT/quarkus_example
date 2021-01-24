fa2daf5b2f5c851e3f3c0d89553d15cf051f7885
Luisito <le.sastre@ebc.edu.mx>
2021-01-24 16:22:51 -0600

    Ading methods for get data from person repository

--- src/main/groovy/jack/service/PersonService.groovy
+++ src/main/groovy/jack/service/PersonService.groovy
@@ -1,9 +1,12 @@
 package jack.service
 
 import jack.command.PersonCommand
+import jack.domain.Person
 
 interface PersonService {
 
   List<PersonCommand> getPeople()
+  PersonCommand getPersonByName(String name)
+  PersonCommand saveNewDeveloper(Person person)
 
 }
--- src/main/groovy/jack/service/PersonServiceImpl.groovy
+++ src/main/groovy/jack/service/PersonServiceImpl.groovy
@@ -1,16 +1,37 @@
 package jack.service
 
 import jack.command.PersonCommand
+import jack.domain.Person
+import jack.repository.PersonRepository
+
+import javax.inject.Inject
 import javax.inject.Singleton
+import javax.transaction.Transactional
 
 @Singleton
-class PersonServiceImpl implements PersonService{
+class PersonServiceImpl implements PersonService {
 
+  @Inject
+  PersonRepository personRepository
+
+  @Override
+  @Transactional
   List<PersonCommand> getPeople() {
-    [new PersonCommand(name: "Luis", lastName: "Sastré", age: 24),
-     new PersonCommand(name: "José", lastName: "Cruz", age: 25),
-     new PersonCommand(name: "Eduardo", lastName: "Peralta", age: 24),
-     new PersonCommand(name: "Rodrigo", lastName: "Martínez", age: 34)]
+    List<Person> people = personRepository.list()
+    people.collect() { PersonCommand.toPersonCommand(it) }
+  }
+
+  @Override
+  @Transactional
+  PersonCommand getPersonByName(String name) {
+    Person person = personRepository.findByName(name)
+    person ? PersonCommand.toPersonCommand(person) : null
+  }
+
+  @Override
+  @Transactional
+  PersonCommand saveNewDeveloper(Person person) {
+    personRepository.persist(person)
   }
 
 }
