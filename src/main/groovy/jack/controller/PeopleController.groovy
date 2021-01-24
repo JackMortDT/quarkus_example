df9df99bfef2743094c4102c36c607a6b8368d66
Luisito <le.sastre@ebc.edu.mx>
2021-01-24 16:22:25 -0600

    Adding new endpoint for save new developer'

--- src/main/groovy/jack/controller/PeopleController.groovy
+++ src/main/groovy/jack/controller/PeopleController.groovy
@@ -1,23 +1,38 @@
 package jack.controller
 
 import jack.command.PersonCommand
+import jack.domain.Person
 import jack.service.PersonService
 
 import javax.inject.Inject
+import javax.ws.rs.Consumes
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/people")
 class PeopleController {
 
   @Inject
   PersonService personService
+  @Inject
 
   @GET
+  @Path("/developers")
   @Produces(MediaType.APPLICATION_JSON)
   List<PersonCommand> getPeople() {
     personService.getPeople()
   }
+
+  @POST
+  @Path("/developers")
+  @Consumes(MediaType.APPLICATION_JSON)
+  Response saveNewDeveloper(Person person) {
+    personService.saveNewDeveloper(person)
+    Response.ok().build()
+  }
+
 }
