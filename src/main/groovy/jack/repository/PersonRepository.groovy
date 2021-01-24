dab1454525cdb7689472d72e4de4be6749e8c855
Luisito <le.sastre@ebc.edu.mx>
2021-01-24 16:28:15 -0600

    Adding new repository for people by PanacheRepository

---
+++ src/main/groovy/jack/repository/PersonRepository.groovy
@@ -0,0 +1,18 @@
+package jack.repository
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository
+import jack.domain.Person
+
+import javax.inject.Singleton
+
+@Singleton
+class PersonRepository implements PanacheRepository<Person> {
+
+  Person findByName(String name) {
+    find("name", name).firstResult()
+  }
+
+  List<Person> list() {
+    listAll()
+  }
+}
