8d801e7d68b3fb251bfb6e0bad281b3f0a86514b
Luisito <le.sastre@ebc.edu.mx>
2021-01-24 21:45:39 -0600

    Creating endpoint that search developers by name

--- src/main/groovy/jack/controller/PeopleController.groovy
+++ src/main/groovy/jack/controller/PeopleController.groovy
@@ -9,6 +9,7 @@ import javax.ws.rs.Consumes
 import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
@@ -25,6 +26,13 @@ class PeopleController {
   @Produces(MediaType.APPLICATION_JSON)
   List<PersonCommand> getPeople() {
     personService.getPeople()
+  }
+
+  @GET
+  @Path("/developers/{name}")
+  @Produces(MediaType.APPLICATION_JSON)
+  List<PersonCommand> getPeopleByName(@PathParam("name") String name) {
+    personService.getPeopleByName(name)
   }
 
   @POST
