8391ae5e65b7a7fdaaeb552ee67c568b6c0526e8
Luisito <le.sastre@ebc.edu.mx>
2021-03-10 09:31:11 -0600

    Validating new endpoint for person and people controller

--- src/main/groovy/jack/controller/PeopleController.groovy
+++ src/main/groovy/jack/controller/PeopleController.groovy
@@ -1,17 +1,14 @@
 package jack.controller
 
-import jack.command.PersonCommand
 import jack.domain.Person
 import jack.service.PersonService
 
-import javax.ws.rs.Consumes
 import javax.ws.rs.GET
-import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Response
 
 @Path("/people")
 class PeopleController {
@@ -25,23 +22,22 @@ class PeopleController {
   @GET
   @Path("/developers")
   @Produces(MediaType.APPLICATION_JSON)
-  List<PersonCommand> getPeople() {
+  List<Person> getPeople() {
     personService.getPeople()
   }
 
   @GET
   @Path("/developers/{name}")
   @Produces(MediaType.APPLICATION_JSON)
-  List<PersonCommand> getPeopleByName(@PathParam("name") String name) {
+  List<Person> getPeopleByName(@PathParam("name") String name) {
     personService.getPeopleByName(name)
   }
 
-  @POST
-  @Path("/developers")
-  @Consumes(MediaType.APPLICATION_JSON)
-  Response saveNewDeveloper(Person person) {
-    personService.saveNewDeveloper(person)
-    Response.ok().build()
+  @GET
+  @Path("/developers_greater_than")
+  @Produces(MediaType.APPLICATION_JSON)
+  List<Person> getPeopleByAgeGreaterThan(@QueryParam("age") Integer age) {
+    personService.findAllByAgeGreaterThan(age)
   }
 
 }
--- src/main/groovy/jack/controller/PersonController.groovy
+++ src/main/groovy/jack/controller/PersonController.groovy
@@ -1,14 +1,17 @@
 package jack.controller
 
 import jack.command.PersonCommand
+import jack.domain.Person
 import jack.service.PersonService
 
 import javax.ws.rs.Consumes
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/person")
 @Produces(MediaType.APPLICATION_JSON)
@@ -23,8 +26,16 @@ class PersonController {
 
   @GET
   @Path("/developer_by_name/{name}")
-  PersonCommand getPersonByName(@PathParam("name") String name) {
+  Person getPersonByName(@PathParam("name") String name) {
     personService.getPersonByName(name)
+  }
+
+  @POST
+  @Path("/developers")
+  @Consumes(MediaType.APPLICATION_JSON)
+  Response saveNewDeveloper(PersonCommand person) {
+    personService.saveNewDeveloper(person)
+    Response.ok().build()
   }
 
 }
